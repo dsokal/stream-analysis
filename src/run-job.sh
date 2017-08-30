32b04f7ddc33eb2c0a84e3780608a33d632bf25f
Dominik Sokal <dominiksokal@gmail.com>
2017-08-30 18:26:43 +0200

    Add cassandra connector for spark. Add usage example.

--- src/run-job.sh
+++ src/run-job.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-$SPARK_HOME/bin/spark-submit --jars \
-  ./external/spark-streaming-kafka-0-8-assembly_2.11-2.2.0.jar \
+$SPARK_HOME/bin/spark-submit \
+  --jars ./external/spark-streaming-kafka-0-8-assembly_2.11-2.2.0.jar \
+  --packages anguenot:pyspark-cassandra:0.5.0 \
+  --conf spark.cassandra.connection.host=cassandra \
   $@
